d0f08a271f78d2f75816ff9fb54789831b2a3a89
almazuulu <almazuulu@gmail.com>
2022-05-31 18:21:20 +0600

    hosting utilities

--- mozio/settings.py
+++ mozio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pc2grphce*l2=3_7q7%hqlkj(9vr%m*td!nj-&f$zjv5fm51qd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '*']
 
 
 # Application definition
